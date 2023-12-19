e67c7c7f87f8aa23ae60155102c3048488d2c751
denniskioko <kiokodennis6990@gmail.com>
2023-12-19 13:00:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride) {
+  if(ride <= 400) {
+    return "This one is on me!";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }else if (ride > 2000 && ride <2500) {
+    return "I will gladly take your thirty bucks.";
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }else if (ride > 2500) {
+    return "No can do.";
+  }
+}
+  
+// Write your code here!
+
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+
+  } else {
+    return "No go.";
+  }
+}
+  
+ // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+
+  } else {
+    return "Bye.";
+  }
+
 }
